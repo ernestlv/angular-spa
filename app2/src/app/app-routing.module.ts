b70d97f0e9df7c853bf047eff812cca90928b0d4
ernestlv <ernestolv@hotmail.com>
2019-06-23 13:54:57 -0400

    SPA example of browser back button bug

--- app2/src/app/app-routing.module.ts
+++ app2/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
-  { path: '**', component: EmptyRouteComponent },
+  { path: 'app2/detail', component: DetailComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
--- app2/src/app/app.module.ts
+++ app2/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmptyRouteComponent
+    HomeComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
